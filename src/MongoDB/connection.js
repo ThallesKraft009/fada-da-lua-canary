4786579664d9670934df865adcdc73708faed9f7
ThallesKraft <99352499+ThallesKraft009@users.noreply.github.com>
2022-12-09 12:36:49 -0300

    Create connection.js

---
+++ src/MongoDB/connection.js
@@ -0,0 +1,26 @@
+module.exports = (client) => {
+
+
+  const c = require("colors")
+
+
+const conectar = async() => {
+
+const { connect } = require("mongoose");
+  
+  
+    await connect(client.config.mongo).then(() => {
+      console.log(c.blue("[INFO]: MongoDB Conectado ✅"));
+      
+    })
+}
+
+
+client.once("ready", () => {
+  conectar()
+})
+
+
+client.userdb = require("./Database/user.js");
+
+ }
