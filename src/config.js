92b93dcbfb73ac8ed0f8c1107370bb9ce876172d
ThallesKraft <99352499+ThallesKraft009@users.noreply.github.com>
2022-12-09 12:35:02 -0300

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  token: "",
+  mongo: "",
+
+  emoji: {
+    ERROR: "❌",
+    SUCCESS: "✅",
+    disabled: "🔴",
+    enabled: "🟢",
+    cleared: "🧹",
+    time: "⏲️",
+    search: "🔎",
+    ping: "🏓",
+    bot: "🤖",
+  },
+  
+  Global: false,
+
+
+  guild: {
+    id: "844581567892291584",
+    logsReaction: "1028756005556846632",
+    estrelinhas: "1028756005556846632"
+  },
+};
